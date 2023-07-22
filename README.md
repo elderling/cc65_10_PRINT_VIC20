ea13ce6e29255cc8e9f71788e66aee378184d124
Jonathan W. Taylor <elderling@gmail.com>
2023-07-22 09:28:16 -0500

    Add code & update README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# cc65_10_PRINT_VIC20
-cc65 VIC-20 Implementation of the famous "10 PRINT" program
+# 10 PRINT in C for Unexpanded VIC-20
+
+C Implementation of the famous "10 PRINT" program for the 
+unexpanded Commodore VIC-20. Currently requires the `cc65` compiler. Compile as follows:
+
+    cl65 -t vic20 ten_print.c -Oi -o ten-print.prg
---
+++ ten_print.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <peekpoke.h>
+
+# define VID_RAM_LAYOUT_REG 0x9005
+
+uint8_t slant1[] = { 'M', 0 };
+uint8_t slant2[] = { 'N', 0 };
+
+uint8_t main(void) {
+
+  // Switch to capitals + graphics character set
+  POKE(VID_RAM_LAYOUT_REG, 0xf0);
+
+  while (1) {
+    (rand() % 2) ? printf("%s", &slant1) : printf("%s", &slant2);
+    //printf("%d", rand() % 2 );
+    //printf("\n");
+  }
+
+  return 0;
+}
+
